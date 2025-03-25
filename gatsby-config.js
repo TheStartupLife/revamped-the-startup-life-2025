ecee03f31bcd86a5f2d36ee30fa6fdc57dfce5fc
Whitney Lubin <justwhitneylubin@gmail.com>
2025-03-25 19:14:14 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `revamped-the-startup-life-2025`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-google-gtag"]
+};
